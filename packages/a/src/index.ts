370f6ad99c77fd1d713f5676e45e7cf5dff7d323
아이작_조서환 <wtae1216@afreecatv.com>
2023-10-20 14:35:05 +0900

    feat(b): add b sub module bug

--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -1,8 +1,10 @@
 import { add } from "@choseohwan-temp/b";
 import { minus } from "@choseohwan-temp/c";
+import { divide } from "@choseohwan-temp/b/divide";
 import { multiply } from "@choseohwan-temp/c/multiply";
 
 const sum = add(1, 2);
 const diff = minus(5, 4);
 const multi = multiply(6, 9);
-console.log(sum, diff, multi);
+const divi = divide(6, 9);
+console.log(sum, diff, multi, divi);
--- packages/c/src/multiply/index.ts
+++ packages/c/src/multiply/index.ts
@@ -1,1 +1,1 @@
-export * from 'src/multiply/multiply.js';
+export * from './multiply';
